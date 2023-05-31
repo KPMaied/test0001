53a480aef6bd951ae25b13f04e7bac221d2f3b92
Siwakorn Mahakan <cardeer.dev@gmail.com>
2023-05-31 20:10:59 +0700

    fix: overflow

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,10 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
+  );
+
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
